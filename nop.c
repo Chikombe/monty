49c8be5ae19b6a97f6463d923edfae5d6fb120ae
chikombe <chikombe.ecy@gmail.com>
2023-08-24 10:24:13 -0700

    nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: pointer to stack
+ * @line_number: the file line where instructions occur
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
