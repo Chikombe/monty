e82111f7014ee0575289bcf2ff9a52e14815fc55
chikombe <chikombe.ecy@gmail.com>
2023-08-25 02:05:47 -0700

    error handling

--- monty.h
+++ monty.h
@@ -41,7 +41,7 @@ typedef struct stack_s
 typedef struct instruction_s
 {
 	char *opcode;
-	void (stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
