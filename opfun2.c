29372fbeeadc6d283439d7c8f8ffc7b32c48b486
LucyKinyanjui <kinyanjuiwambui86@gmail.com>
2023-08-25 05:09:40 -0700

    sub and some few files add.

--- opfun2.c
+++ opfun2.c
@@ -53,7 +53,7 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (!*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n",
+		fprintf(stderr, "L%u: can't sub, stack too short\n",
 			line_number);
 		all_free();
 		exit(EXIT_FAILURE);
